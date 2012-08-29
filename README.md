eeb9ee8e9632849070135c462e6f4666b4e3ef28
elemc <me@elemc.name>
2012-08-29 13:24:34 +0400

    Added main script

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ packages-list
 =============
 
 Created for RF#831
+
+Change with_auth=1, github_username and github_password for authentication in github.
+And run script.
---
+++ packages-list.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+
+readme_file="README.RFRemix"
+temp="/tmp/github-repos"
+result_file="${temp}/result.txt"
+
+github_username=""
+github_password=""
+with_auth=0
+github_api_url="https://api.github.com/orgs/RussianFedora/repos"
+list_file="${temp}/list.txt"
+
+function read_readme_file {
+    dir=$1
+    echo "${dir}:" >> $result_file
+    while read line; do
+        echo -e "\t${line}" >> $result_file
+    done < $readme_file
+}
+
+if [ -d $temp ]; then
+    pushd $temp
+else
+    mkdir -p $temp || exit 1
+    pushd $temp
+fi
+
+# Download all repository
+rm -rf list.txt
+if [ $with_auth -lt 1 ]; then
+    curl -i ${github_api_url} > $list_file
+else
+    curl -u "${github_username}:${github_password}" -i ${github_api_url} > $list_file
+fi
+repos=`cat list.txt | grep git_url | awk '{print $2}' | sed 's|["\,]||g' | sort`
+for repo in $repos; do
+    git clone $repo
+done
+
+# Remove result_file
+if [ -f $result_file ]; then
+    rm -rf $result_file
+fi
+
+# Check README.RFRemix
+dirs=`find . -maxdepth 1 -type d`
+for dir in $dirs; do
+    if [ "$dir" == "." ]; then
+        continue
+    fi
+    dir_name=`echo $dir | cut -c 3-`
+    pushd $dir
+
+    found="NO"
+    if [ -f $readme_file ]; then
+        read_readme_file $dir_name
+        found="YES"
+    else
+        branches=`git branch -a | grep -vE "HEAD|\*|bz" | sed 's|remotes/origin/||g'`
+        for branch in $branches; do
+            git checkout $branch > /dev/null 2>&1
+            if [ -f $readme_file ]; then
+                read_readme_file $dir_name
+                found="YES"
+                break
+            fi
+        done
+    fi
+
+    if [ "$found" == "NO" ]; then
+        echo -e "${dir_name}: Файл описания не найден!" >> $result_file
+    fi
+    popd
+done
+    
+popd
